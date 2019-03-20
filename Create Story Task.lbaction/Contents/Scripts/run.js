9dae5506213534205b36a7fe5a6f73a31a1bd643
Wang Chen <wangchen@youzan.com>
2019-03-20 16:57:44 +0800

    omnifocus 新增设置截止日功能

--- Create Story Task.lbaction/Contents/Scripts/run.js
+++ Create Story Task.lbaction/Contents/Scripts/run.js
@@ -1,7 +1,44 @@
 function runWithString(query) {
 	if (!query) return
 
-	const url = encodeURI(`omnifocus:///paste?target=/folder/c3fIdj74hVt&content=${query}: @autodone(true) @parallel(false) @due(2019-03-10 17:00)\n\t- 开发\n- 自测\n- 登记\n- 上车\n- Review\n- 验证\n- 发布\n- 回归`)
+	const result = [
+		{
+			title: '创建',
+			action: 'create',
+			actionArgument: query
+		}
+	]
+
+	const deadlineDate = query.match(/(\d{2,4}\-){1,2}\d{2}/) && query.match(/(\d{2,4}\-){1,2}\d{2}/)[0]
+	const deadlineTime = query.match(/\d{2}\:\d{2}/) && query.match(/\d{2}\:\d{2}/)[0]
+
+	if (deadlineDate || deadlineTime) {
+		if (deadlineDate) query = query.replace(deadlineDate, '')
+		if (deadlineTime) query = query.replace(deadlineTime, '')
+		
+		result.push({
+			title: '创建并设置截止日',
+			subtitle: deadlineDate + deadlineTime,
+			action: 'createWithDeadline',
+			actionArgument: {
+				query,
+				deadlineDate,
+				deadlineTime
+			}
+		})
+	}
+
+	return result
+}
+
+function create(query) {
+	const url = encodeURI(`omnifocus:///paste?target=/folder/c3fIdj74hVt&content=${query}: @autodone(true) @parallel(false)\n\t- 开发\n- 自测\n- 登记\n- 上车\n- Review\n- 验证\n- 发布\n- 回归`)
 	
+	LaunchBar.openURL(url)
+}
+
+function createWithDeadline({ query, deadlineDate, deadlineTime = '21:00' }) {
+	const url = encodeURI(`omnifocus:///paste?target=/folder/c3fIdj74hVt&content=${query}: @autodone(true) @parallel(false) @due(${deadlineDate || new Date().toLocaleDateString().replace(/\//g, '-')} ${deadlineTime})\n\t- 开发\n- 自测\n- 登记\n- 上车\n- Review\n- 验证\n- 发布\n- 回归`)
+
 	LaunchBar.openURL(url)
 }
