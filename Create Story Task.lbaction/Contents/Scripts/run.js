f39b95ffdea14fb9374c15bc9e8b18a66bef6fad
vvangchen <hello@wchen.me>
2019-04-03 23:09:04 +0800

    add note feature

--- Create Story Task.lbaction/Contents/Scripts/run.js
+++ Create Story Task.lbaction/Contents/Scripts/run.js
@@ -1,11 +1,26 @@
 function runWithString(query) {
 	if (!query) return
 
+	// 效率地址
+	const noteXL = query.match(/xl\((.+?)\)/) && query.match(/xl\((.+?)\)/)[1]
+	if (noteXL) {
+		query = query.replace(/xl\((.+?)\)/, '')
+	}
+	// 分支
+	const noteB = query.match(/b\((.+?)\)/) && query.match(/b\((.+?)\)/)[1]
+	if (noteB) {
+		query = query.replace(/b\((.+?)\)/, '')
+	}
+
 	const result = [
 		{
 			title: '创建',
 			action: 'create',
-			actionArgument: query
+			actionArgument: {
+				query,
+				noteXL,
+				noteB
+			}
 		}
 	]
 
@@ -44,8 +59,8 @@ function runWithString(query) {
 	return result
 }
 
-function create(query) {
-	const url = encodeURI(`omnifocus:///paste?target=/folder/c3fIdj74hVt&content=${query}: @autodone(true) @parallel(false)\n\t- 开发\n- 自测\n- 登记\n- 上车\n- Review\n- 大巴车验证\n- master验证\n- 发布\n- 回归`)
+function create({ query, noteXL = '', noteB = '' }) {
+	const url = encodeURI(`omnifocus:///paste?target=/folder/c3fIdj74hVt&content=${query}: @autodone(true) @parallel(false)${ noteB ? '\n分支：' + noteB : '' }${ noteXL ? '\n效率地址：' + noteXL : '' }\n\t- 开发\n- 自测\n- 登记\n- 上车\n- Review\n- 大巴车验证\n- master验证\n- 发布\n- 回归`)
 	
 	LaunchBar.openURL(url)
 }
